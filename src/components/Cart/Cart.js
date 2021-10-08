2460b6a09ba715bbf884550deef2af51d680aa2f
Franco Traverso <franco-t20@outlook.com>
2021-10-08 12:26:55 -0300

    define item add and removal functions, passed through props to the CartItem component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,10 +11,21 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0; //if the cart has items, true, otherwise, false
 
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-item"]}>
       {cartCtx.items.map((item) => (
-        <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} />
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
